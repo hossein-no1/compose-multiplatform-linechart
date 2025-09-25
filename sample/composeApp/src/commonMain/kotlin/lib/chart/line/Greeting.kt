eab3a8fae88baf033bb5923fb82c4a3276e2a3f8
hosen <hpleo0938@gmail.com>
2025-09-25 15:29:19 +0330

    remove useless codes

--- sample/composeApp/src/commonMain/kotlin/lib/chart/line/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package lib.chart.line
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- sample/composeApp/src/commonMain/kotlin/lib/chart/line/Platform.kt
+++
@@ -1,7 +0,0 @@
-package lib.chart.line
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
